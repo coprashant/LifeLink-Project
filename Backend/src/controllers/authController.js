15dae9277ec6c9643891029a1cc7a5bbbea19a65
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-01-23 14:03:31 +0545

    Used bcryptjs instead bcrypt

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
 const db = require('../config/db'); 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 exports.handleLogin = async (req, res) => {
     const { role, email, password } = req.body;
