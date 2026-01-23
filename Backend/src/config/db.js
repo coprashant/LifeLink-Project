c37e521327de943c6d674ad7f234ad497a87706a
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-01-23 13:54:48 +0545

    Improvised on db.js to give access to the neondb

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -8,6 +8,10 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
+
+  ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // Test the connection immediately on startup
