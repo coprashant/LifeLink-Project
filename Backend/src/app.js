8b6d69c6b9799aaace442c81861754f24a6019ae
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-01-29 21:43:06 +0545

    Fixed bugs in styling the page

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../../Frontend/HTML')));
+app.use(express.static(path.join(__dirname, '../../Frontend')));
 
 app.use(session({
     secret: process.env.SESSION_SECRET || 'thapathali_campus_key',
