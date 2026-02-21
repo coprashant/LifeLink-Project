4c38c99776e54eb113ff4a49bde9e0f631c86cb6
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-02-21 17:07:10 +0545

    fixed bug

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(cors({
-  origin: isProduction ? process.env.FRONTEND_URL : 'http://localhost:5173',
+  origin: isProduction ?[process.env.FRONTEND_URL, "https://lifelink-project.vercel.app"] : 'http://localhost:5173',
   credentials: true
 }));
 
