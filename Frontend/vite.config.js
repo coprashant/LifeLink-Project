5a90ae4b630e969db5d30d00a141c4c5317fcc23
Prasant Bhattarai <bhattaraiprashant010@gmail.com>
2026-02-22 12:31:07 +0545

    Fix: Vercel errors

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-
   base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@vitejs/plugin-react': path.resolve(__dirname, '../node_modules/@vitejs/plugin-react')
+    }
   },
   server: {
     proxy: {
